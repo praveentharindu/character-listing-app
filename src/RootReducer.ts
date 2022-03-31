56a992803d823846a804fb88be25dda1f36a7ad1
Praveen Tharindu <praveentharindu@Praveens-MacBook-Air-2.local>
2022-03-31 17:43:37 +0530

    add husky

--- src/RootReducer.ts
+++ src/RootReducer.ts
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux'
 import CharactersReducer from './features/characters/CharactersReducer'
 
-const RootReducer = combineReducers({
-  characters: CharactersReducer,
-})
 
 export default RootReducer
